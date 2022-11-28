74262300f86a59edb6129db9d84f78598506dc71
Guzl89 <gulnazfazlieva@gmail.com>
2022-11-28 22:31:46 +0300

    added new tasks

--- curriculum/js/script.js
+++ curriculum/js/script.js
@@ -55,3 +55,111 @@ function caseInSwitch(val) {
 }
 
 console.log(caseInSwitch(4));
+
+//switch exircise2
+
+/*Write a switch statement to set answer for the following conditions:
+a - apple
+b - bird
+c - cat
+default - stuff
+*/
+
+function switchOfStuff(val) {
+  let answer = "";
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+  }
+
+  return answer;
+}
+
+console.log(switchOfStuff(1));
+
+//switch exircise3
+
+/*Write a switch statement to set answer for the following ranges:
+1-3 - Low
+4-6 - Mid
+7-9 - High
+Note: You will need to have a case statement for each number in the range.
+*/
+
+function sequentialSizes(val) {
+  let answer = "";
+
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+  return answer;
+}
+
+console.log(sequentialSizes(1));
+
+//switch statement
+//Change the chained if/else if statements into a switch statement.
+function chainToSwitch(val) {
+  let answer = "";
+  if (val === "bob") {
+    answer = "Marley";
+  } else if (val === 42) {
+    answer = "The Answer";
+  } else if (val === 1) {
+    answer = "There is no #1";
+  } else if (val === 99) {
+    answer = "Missed me by this much!";
+  } else if (val === 7) {
+    answer = "Ate Nine";
+  }
+  return answer;
+}
+chainToSwitch(7);
+
+//solution
+
+function chainToSwitch(val) {
+  let answer = "";
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  return answer;
+}
+console.log(chainToSwitch(7));
