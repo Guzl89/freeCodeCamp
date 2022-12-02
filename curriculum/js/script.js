77ecab9a6830860b035339c44c063866927a7156
Guzl89 <gulnazfazlieva@gmail.com>
2022-12-02 14:14:01 +0300

    solutio func switch

--- curriculum/js/script.js
+++ curriculum/js/script.js
@@ -163,3 +163,64 @@ function chainToSwitch(val) {
   return answer;
 }
 console.log(chainToSwitch(7));
+
+//Counting Cards
+
+let count = 0;
+
+function cc(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count += 1;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      count += 0;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count -= 1;
+      break;
+  }
+  if (count <= 0) {
+    return count + " Hold";
+  } else {
+    return count + " Bet";
+  }
+  return count;
+}
+
+cc(2);
+cc(3);
+cc(7);
+cc("K");
+cc("A");
+
+//Build JavaScript Objects
+/*Make an object that represents a dog called myDog which contains the properties name (a string), 
+legs, tails and friends.You can set these object properties to whatever values you want, as long 
+as name is a string, legs and tails are numbers, and friends is an array*/
+
+const myDog = {
+"name": "Karl",
+"legs": 4,
+"tails": 1,
+"friends": [10]
+};
+
+//Accessing Object Properties with Dot Notation
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+const hatValue = testObj.hat;     
+const shirtValue = testObj.shirt;   
