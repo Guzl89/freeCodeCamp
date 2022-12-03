2e552569229bb2900b73ea316a35ee66339e1847
Guzl89 <gulnazfazlieva@gmail.com>
2022-12-03 18:12:27 +0300

    new solutions

--- curriculum/js/codewarssolutions.js
+++ curriculum/js/codewarssolutions.js
@@ -6,20 +6,20 @@ Given [34, -345, -1, 100] your solution will return -345
 You can assume, for the purpose of this kata, that the supplied array will not be empty.*/
 
 class SmallestIntegerFinder {
-    findSmallestInt(args) {
-   var temp  ;
-  
-     for (var i=0; i<args.length; i++){
-         if(args[i] <= args[0]){
-             args[0]  = args[i];    
-             temp = args[i];
-         }
-       }
-     return temp;
-   }
- }
+  findSmallestInt(args) {
+    var temp;
 
- /* Your task is to create a function that does four basic mathematical operations.
+    for (var i = 0; i < args.length; i++) {
+      if (args[i] <= args[0]) {
+        args[0] = args[i];
+        temp = args[i];
+      }
+    }
+    return temp;
+  }
+}
+
+/* Your task is to create a function that does four basic mathematical operations.
 The function should take three arguments - operation(string/char), value1(number), value2(number).
 The function should return result of numbers after applying the chosen operation.
 Examples(Operator, value1, value2) --> output
@@ -28,22 +28,42 @@ Examples(Operator, value1, value2) --> output
 ('*', 5, 5) --> 25
 ('/', 49, 7) --> 7*/
 
-function basicOp(operation, value1, value2)
-{
+function basicOp(operation, value1, value2) {
   let answer = 0;
-  switch(operation){
-      case '+':
-        answer = value1 + value2;
-        break;
-      case '-':
-        answer = value1 - value2;
-        break;
-      case '*':
-        answer = value1 * value2;
-        break;
-      case '/':
-        answer = value1 / value2;
-        break;
+  switch (operation) {
+    case "+":
+      answer = value1 + value2;
+      break;
+    case "-":
+      answer = value1 - value2;
+      break;
+    case "*":
+      answer = value1 * value2;
+      break;
+    case "/":
+      answer = value1 / value2;
+      break;
   }
   return answer;
 }
+
+/* Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+Use conditionals to return the proper message:
+case	             return
+name equals owner	'Hello boss'
+otherwise	        'Hello guest'*/
+
+function greet(name, owner) {
+  if (name === owner) {
+    return "Hello boss";
+  } else {
+    return "Hello guest";
+  }
+}
+
+//MakeUpperCase
+function makeUpperCase(str) {
+    return str.toUpperCase();
+}
+    const result = makeUpperCase('Hello everybody');
+    console.log(result);
