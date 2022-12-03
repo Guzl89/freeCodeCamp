be463e1b4dee0e3710fce35379367be7446d8057
Guzl89 <gulnazfazlieva@gmail.com>
2022-12-03 17:00:14 +0300

    added new solution

--- curriculum/js/script.js
+++ curriculum/js/script.js
@@ -224,3 +224,32 @@ const testObj = {
 };
 const hatValue = testObj.hat;     
 const shirtValue = testObj.shirt;   
+
+//Accessing Object Properties with Bracket Notation
+const testObj1 = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+const entreeValue = testObj1["an entree"];  
+const drinkValue = testObj1["the drink"];  
+
+//Accessing Object Properties with Variables
+const testObj2 = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+const playerNumber = 16; 
+const player = testObj2[playerNumber];   
+console.log(player);
+
+//Add New Properties to a JavaScript Object
+const myDog1 = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+myDog1.bark = "woof";
+
