156aa3b71e6c21b8e221f2ba6535c90d358ebf84
Guzl89 <gulnazfazlieva@gmail.com>
2022-12-05 19:47:16 +0300

    added new solution

--- curriculum/js/codewarssolutions.js
+++ curriculum/js/codewarssolutions.js
@@ -63,7 +63,15 @@ function greet(name, owner) {
 
 //MakeUpperCase
 function makeUpperCase(str) {
-    return str.toUpperCase();
+  return str.toUpperCase();
 }
-    const result = makeUpperCase('Hello everybody');
-    console.log(result);
+const result = makeUpperCase("Hello everybody");
+console.log(result);
+
+/* Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. 
+If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.*/
+function lovefunc(flower1, flower2) {
+  return flower1 % 2 !== flower2 % 2;
+}
+
